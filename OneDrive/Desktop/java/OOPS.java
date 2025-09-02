3094ce1b2b0b5ffc7437d21b8313b305ad6d6bda
Karnsinghgolu <karnsinghgolu@gmail.com>
2025-09-02 17:00:11 +0530

    javabasic

---
+++ OneDrive/Desktop/java/OOPS.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class OOPS {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b =sc.nextInt();
+        System.out.println(a+b);
+        // calculator cal=new calculator();
+        // System.out.println(cal.sum(1,2));
+        //         System.out.println(cal.sum((float)1.3, (float)2.4));
+        // System.out.println(cal.sum(1,2,3));
+
+        
+    }
+    
+}
+// class calculator{
+//     int sum(int a,int b){
+//         return a+b;
+//     }
+//     float sum(float a,float b){
+//         return a+b;
+//     }
+//     int sum(int a,int b,int c){
+//      return a+b+c;
+//     }
+// }
